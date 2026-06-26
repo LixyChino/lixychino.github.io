445557c3b8699c7b073b4973edda82948f8e33b9
東風谷りくしぃ - KochiyaLixy <108574061+LixyChino@users.noreply.github.com>
2026-06-26 14:01:27 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const button = document.getElementById('oldpage');
 button.setAttribute('onclick', 'closemsg()');
 
-function closemsg() {
-    button.classList.add('hid');
-    document.getElementById('header').classList.add('msgclose');
+function closemsg(e) {
+    const evt = e || window.event;
+    evt.target.classList.add('hid');
+    document.getElementById('oldpage').classList.add('msgclose');
 }
